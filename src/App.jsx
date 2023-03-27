dbfcc08e401148a72db0d095b0048e84dc88ac74
name <email@email.com>
2023-03-27 15:43:40 -0300

    Entrega em correção4

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Header} from './Components/components/Header/header'
-import { Form } from "./Components/components/Form/form"
+import { Form } from './Components/components/Form/form'
 import { ResFinan } from './Components/components/resFinan'
 import './index.css'
 import { ValorTotal } from './Components/components/valorTotal'
