623e870706c5a5e8609c79bb34dc9508bc58817d
name <email@email.com>
2023-03-27 16:20:36 -0300

    Entrega a ser corrigida

--- src/Components/components/Header/header.jsx
+++ src/Components/components/Header/header.jsx
@@ -1,8 +1,11 @@
+import imagem from '../../../assets/Nu Kenzie.png'
+
+
 export function Header() {
     return(
         <>
 <header className='cabecalho'>
-    <img className="logo" src={'https://github.com/Kenzie-Academy-Brasil-Developers/https-github.com-Kenzie-Academy-Brasil-Developers-react-entrega-s1-template-nu-kenzie-m1342s-/blob/b064a9feecd2f136de4386d2b2e03b2df9d2947f/src/assets/Nu%20Kenzie.png'} alt="Logo Nu Kenzie"/>
+    <img className="logo" src={imagem} alt="Logo Nu Kenzie"/>
 </header>
     <hr/>
     </>
